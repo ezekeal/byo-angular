4d6ee92a71eec0b865c01f92eb441916202759c9
Zach Koch <ezekeal@gmail.com>
2015-03-04 00:24:15 -0800

    dirty checking continues until variables are unchanged

--- test/scope_spec.js
+++ test/scope_spec.js
@@ -63,5 +63,67 @@ describe("Scope", function() {
             expect(scope.counter).toBe(2);
 
         });
+
+        it("calls listener when watch value is first undefined", function() {
+            scope.counter = 0;
+
+            scope.$watch(
+                function(scope) { return scope.someValue; },
+                function(newValue, oldValue, scope) {scope.counter++; }
+            );
+
+            scope.$digest();
+            expect(scope.counter).toBe(1);
+        });
+
+        it("calls listener with new value as old value the first time", function() {
+            scope.someValue = 123;
+            var oldvalueGiven;
+
+            scope.$watch(
+                function(scope) { return scope.someValue; },
+                function(newValue, oldValue, scope) { oldvalueGiven = oldValue; }
+            );
+
+            scope.$digest();
+            expect(oldvalueGiven).toBe(123);
+        });
+
+        it("may have watchers that omit the listener function", function() {
+            var watchFn = jasmine.createSpy().and.returnValue('something');
+            scope.$watch(watchFn);
+
+            scope.$digest();
+
+            expect(watchFn).toHaveBeenCalled();
+        });
+
+        it("triggers chained watchers in the same digest", function() {
+            scope.name = 'Jane';
+
+            scope.$watch(
+                function(scope) { return scope.nameUpper; },
+                function(newValue, oldValue, scope) {
+                    if (newValue) {
+                        scope.initial = newValue.substring(0, 1) + '.';
+                    }
+                }
+            );
+
+            scope.$watch(
+                function(scope) { return scope.name; },
+                function(newValue, oldValue, scope) {
+                    if (newValue) {
+                        scope.nameUpper = newValue.toUpperCase();
+                    }
+                }
+            );
+
+            scope.$digest();
+            expect(scope.initial).toBe('J.');
+            scope.name = 'Bob';
+            scope.$digest();
+            expect(scope.initial).toBe('B.');
+        });
     });
 });
